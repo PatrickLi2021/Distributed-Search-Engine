b128eda1dc15ac5878b7191eff54131644a080b8
PatrickLi2021 <patrickli20212@gmail.com>
2025-01-28 01:51:29 +0000

    all original tests pass with 3 components converted to .js scripts

---
+++ non-distribution/c/invert.js
@@ -0,0 +1,21 @@
+const URL = process.argv[2];
+const terms = process.argv[3]; 
+
+const countOccurrences = (terms, url) => {
+  const termArray = terms.split('\n')
+      .map((term) => term.trim())
+      .filter((term) => term !== '');
+
+  const termCounts = {};
+  termArray.forEach((term) => {
+    termCounts[term] = (termCounts[term] || 0) + 1;
+  });
+
+  const formattedOutput = Object.entries(termCounts)
+      .map(([term, count]) => `${term} | ${count} | ${url}`)
+      .join('\n');
+
+  return formattedOutput;
+};
+
+console.log(countOccurrences(terms, URL));
