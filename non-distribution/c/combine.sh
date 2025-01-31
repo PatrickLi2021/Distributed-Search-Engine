10fd39d22642a41eca6e32ec8bd7fc0c7c9e4ce3
PatrickLi2021 <patrickli20212@gmail.com>
2025-01-31 04:44:55 +0000

    stabilizing changes again

--- non-distribution/c/combine.sh
+++ non-distribution/c/combine.sh
@@ -3,14 +3,10 @@
 # Combine terms to create  n-grams (for n=1,2,3) and then count and sort them
 # Usage: ./combine.sh <terms > n-grams
 
-p1=$(mktemp -u p1.XXXXXX)
-p2=$(mktemp -u p2.XXXXXX)
-p3=$(mktemp -u p3.XXXXXX)
+input=$(cat)
 
-mkfifo "$p1" "$p2" "$p3"
-
-tee >(sort) >(tee "$p1" | tail +2 | paste "$p1" - | sort) >(tee "$p2" | tail +2 | paste "$p2" - | tee "$p3" | cut -f 1 | tail +3 | paste "$p3" - | sort) > /dev/null
-
-rm "$p1" "$p2" "$p3"
-
-tee >(sort) >(bigram "$p1") >(trigram "$p2" "$p3") > /dev/null
+if [[ "$(pwd)" =~ /t$ ]]; then
+    node ../c/combine.js "$input"
+else
+    node c/combine.js "$input"
+fi
