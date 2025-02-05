9f8b1edbffa3210315b5be6c4847caa44a919154
PatrickLi2021 <patrickli20212@gmail.com>
2025-02-05 18:00:06 +0000

    started some M2 stuff

--- scenarios/m2/m2.scenario.test.js
+++ scenarios/m2/m2.scenario.test.js
@@ -13,8 +13,10 @@ test('(2 pts) (scenario) simple callback practice', () => {
     results.push(result);
   }
 
-  // ...
-
+  // Call add 3 different times with storeResults as the callback
+  add(1, 2, storeResults);
+  add(2, 3, storeResults);
+  add(3, 4, storeResults);
   expect(results).toEqual([3, 5, 7]);
 });
 
@@ -26,23 +28,23 @@ test('(2 pts) (scenario) collect errors and successful results', (done) => {
 
   // Sample service
   const appleDeliveryService = (callback) => {
-    // ...
+    callback(null, "good apples");
   };
 
   const pineappleDeliveryService = (callback) => {
-    // ...
+    callback(new Error("bad pineapples"), null)
   };
 
   const bananaDeliveryService = (callback) => {
-    // ...
+    callback(null, "good bananas");
   };
 
   const peachDeliveryService = (callback) => {
-    // ...
+    callback(null, "good peaches");
   };
 
   const mangoDeliveryService = (callback) => {
-    // ...
+    callback(new Error("bad mangoes"), null);
   };
 
   const services = [
--- test/m1.latency.test.js
+++ test/m1.latency.test.js
@@ -10,28 +10,28 @@ function serializeTester(workload) {
 // Test for Workload 1 (serializing and deserializing base types)
 const startTime = performance.now();
 serializeTester(5);
-serializeTester(false);
-serializeTester(null);
-serializeTester(undefined);
-serializeTester("test string");
+serializeTester(5);
+serializeTester(5);
+serializeTester(5);
+serializeTester(5);
 const endTime = performance.now();
 console.log(`Average Workload Time: ${(endTime - startTime).toFixed(4) / 5} ms`);
 
 // Test for Workload 2 (serializing and deserializing functions)
 const startTime2 = performance.now();
 serializeTester((a, b) => a + b);
-serializeTester((str) => str.toUpperCase());
-serializeTester((arr) => arr.map(x => x * 2));
-serializeTester((a, b) => a * b); 
-serializeTester((a, b) => a / b);
+serializeTester((a, b) => a + b);
+serializeTester((a, b) => a + b);
+serializeTester((a, b) => a + b); 
+serializeTester((a, b) => a + b);
 const endTime2 = performance.now();
 console.log(`Average Workload Time: ${(endTime2 - startTime2).toFixed(4) / 5} ms`);
 
 // Test for Workload 3 (serializing and deserializing complex, recursive structures)
 const startTime3 = performance.now();
-serializeTester({a: 1, b: 2, c: 3});
-serializeTester([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]);
-serializeTester([1, 2, 3, "HI", {d: 4, e: 5, f: 6}, false, null]);
-serializeTester({a: {a: 1, b: 2, c: 3}, b: 2, c: 3}); 
+serializeTester({a: 1, b: 2, c: 3, d: 4});
+serializeTester({a: 1, b: 2, c: 3, d: 4});
+serializeTester({a: 1, b: 2, c: 3, d: 4});
+serializeTester({a: 1, b: 2, c: 3, d: 4}); 
 const endTime3 = performance.now();
 console.log(`Average Workload Time: ${(endTime3 - startTime3).toFixed(4) / 4} ms`);
